bcc87eea522a66ffec24d8cd4b1d861c7c21f246
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-09-23 23:33:26 +0200

    Task 1 and Task 2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// The Car class has two properties: Licenseplate, Date. It has two methods: Price(), VehicleType(). 
+    /// </summary>
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Constructor to initialize all the properties
+        /// </summary>
+        /// <param name="licenseplate">Initializes Licenseplate property</param>
+        /// <param name="date">Initializes Date property</param>
+        public Car(string licenseplate, DateTime date)
+        {
+            Licenseplate = licenseplate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public Car()
+        {
+        }
+
+        /// <summary>
+        /// Price() method returns a fixed price 240
+        /// </summary>
+        /// <returns>Double</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            var result = car.Price();
+
+            //Assert
+            Assert.AreEqual(240, result);
+
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            var result = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Car", result);
+
+        }
+    }
+}
